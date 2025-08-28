d793ad24740db15e18176ffe67d147c1db714162
Jamal Kharrat <jamal7@me.com>
2025-08-27 20:31:05 -0500

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,74 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { env } from 'cloudflare:workers';
+import { WorkerMailer } from 'worker-mailer';
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		if (request.method === 'OPTIONS') {
+			// Handle CORS preflight
+			return new Response(null, {
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'POST, OPTIONS',
+					'Access-Control-Allow-Headers': 'Content-Type',
+				},
+			});
+		}
+
+		if (request.method === 'POST') {
+			try {
+				const data = await request.json<{
+					name?: string;
+					email?: string;
+					message?: string;
+				}>();
+
+				if (!data.name || !data.email || !data.message) {
+					return new Response(JSON.stringify({ error: 'Missing required fields' }), {
+						status: 400,
+						headers: { 'Content-Type': 'application/json' },
+					});
+				}
+
+				// Craft the email
+				const mailer = await WorkerMailer.connect({
+					credentials: {
+						username: 'feedback@beroea.tech',
+						password: env.pass,
+					},
+					authType: 'plain',
+					host: 'smtp.migadu.com',
+					port: 465,
+					secure: true,
+				});
+
+				await mailer.send({
+					from: { name: 'Feedback', email: 'feedback@beroea.tech' },
+					to: { name: 'Jamal', email: 'jamal@beroea.tech' },
+					subject: `Feedback from ${data['name']}`,
+					text: `Message: ${data['message']} \n\n ${data['email']}`,
+					html: `<p>Feedback content:</p> <p>${data['message']}</p> <p>Sender: ${data['email']}</p>`,
+				});
+
+				mailer.close();
+
+				return new Response(JSON.stringify({ success: true, received: data }), {
+					status: 200,
+					headers: {
+						'Content-Type': 'application/json',
+						'Access-Control-Allow-Origin': '*', // allow frontend calls
+					},
+				});
+			} catch (err) {
+				return new Response(JSON.stringify({ error: `Invalid JSON ${err}` }), {
+					status: 400,
+					headers: { 'Content-Type': 'application/json' },
+				});
+			}
+		}
+
+		return new Response('Method Not Allowed', { status: 405 });
 	},
-} satisfies ExportedHandler<Env>;
+};
